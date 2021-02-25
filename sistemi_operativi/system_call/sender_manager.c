c5664c32b8387832171ab06d47cbee520429d492
Filippo-Peret <filippoperettivr@libero.it>
2021-02-25 17:10:48 +0100

    Sender manger - ci siamo quasi!

--- sistemi_operativi/system_call/sender_manager.c
+++ sistemi_operativi/system_call/sender_manager.c
@@ -26,12 +26,13 @@
 
 //TYPEDEF
 typedef struct message_info{
-    int id;
+    int  id;
     char *message;
-    char id_sender[ID];
+    char *id_sender;
+    char *id_reciver;
     int del_S1;
     int del_S2;
-    int del_s2;
+    int del_S3;
     char *type;
 }message_info_t;
 
@@ -50,57 +51,138 @@ typedef struct sender_traffic_info{
     time_t time_depart;
 }sender_traffic_info_t;
 
+
+void startprocess(int signal){
+    printf("Start process ");
+}
+
 //FUNCTION
 
 int lenghtLine(int fd);
 int openFile(char pathFile[], mode_t mode);
-void readLine(int fd);
+char* readLine(int fd);
 void writeLine(int fd, char string[]);
 int lenghtString(char string[]);
 void writeProcessInfo(int fd, char name[], int pid);
 char* intToChar(int pid);
+char* readInformation(int *index, int *lenght, char* info);
+int charToInt(char* string, int *lenght);
 
+message_info_t getInformationByLine(char* line);
 int main(int argc, char * argv[]) {
-
+    //VARIABLES TO SAVE INFORMATION MESSAGE
+    message_info_t sendToS2;
+    message_info_t sendToS3;
     //VARIABLES TO OPEN FILE
-    char path[]="./InputFiles/F0.csv";
-    char pathFileS[]="./OutputFiles/FX.csv";
-    mode_t modeO=O_RDWR; // open
-    mode_t modeC=S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH; // create
+    char pathFileI[]="./InputFiles/FX.csv";
+    char pathFileO[]="./OutputFiles/FX.csv";
+    mode_t modeO=O_RDWR; //OPEN
+    mode_t modeC=S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH; //CREATE
     int fileDesc;
     
-    //READ FILE F8.csv IF EXIST OR CREATE THEN
-    pathFileS[15]='7';
-    fileDesc=openFile(pathFileS,modeO);
+
+    //OPEN FILE F8.csv IF EXIST-OR CREATE THEN
+    pathFileO[15]='7';
+    fileDesc=openFile(pathFileO,modeO);
     if(fileDesc==-1){
         printf("File not Exist..\nCreate a new file F8.csv");
-        fileDesc=creat(pathFileS,modeC);
+        fileDesc=creat(pathFileO,modeC);
         char head[]="Sender Id;PID\n";
         writeLine(fileDesc, head);
+        close(fileDesc);
     }
 
-    //OPEN INPUT FILE
-    fileDesc=openFile(path,modeO);
-    if(fileDesc==-1){
-        _exit(1);//File not Found
-    }
-
-    //READ FIRST LINE (DESCRIPTION)
-    readLine(fileDesc);
-    
-    int S1;
-    
-    //SUSPEND PARENT PROCESS
-    wait(0);
-
-    //CREATE A CHILD PROCESS S1
+    //CREATE CHILD PROCESS S1
+    pid_t S1;
     if((S1=fork())==0){
-        //ADD PROCESS IN F8.csv
-        pathFileS[15]='7';
-        fileDesc=openFile(pathFileS, modeO);
+        signal(SIGCONT,startprocess);
+        pause();
+        printf("S1...\n");
+        pathFileO[15]='1';
+        pathFileI[14]='0';
+        int fileDescI;
+        if(openFile(pathFileO,modeO)==-1){//IF FILE NOT EXIST
+            char firstLine[]="Id;Message;Id Sender;Id Reciver;Time arrival;Time Depart.\n";
+            fileDesc=creat(pathFileO,modeC);
+            writeLine(fileDesc,firstLine);
+            close(fileDesc);
+        }
+        message_info_t mesInfo;
+        fileDesc=openFile(pathFileO,modeO);
+        fileDescI=openFile(pathFileI, modeO);
+        readLine(fileDescI);
+        char* info=readLine(fileDescI);
+        printf("Reading information...");
+        printf("%s",info);
+        mesInfo=getInformationByLine(info);
+        if(mesInfo.id_sender[1]=='2'){
+            sendToS2=mesInfo;
+        }
+        else if(mesInfo.id_sender[1]=='3'){
+            sendToS3=mesInfo;
+        }
+        
+        exit(0);
+    }
+    else{
+        //WRITE PROCESS ID ON FILE BY PARENT (SENEDER MANAGER)
+        if(S1==-1){
+            printf("Error Create a process");
+            _exit(1);
+        }
+        printf("Process ID Parents: %i\n", getpid());
+        pathFileO[15]='7';
+        fileDesc=openFile(pathFileO, modeO);
         lseek(fileDesc,0L,SEEK_END);
         char pname[]="S1";
         writeProcessInfo(fileDesc,pname,S1);
+        close(fileDesc);
+    }
+
+    //CREATE CHILD PROCESS S2 
+    pid_t S2;
+    if((S2=fork())==0){
+        pause();
+        exit(0);
+    }
+    else{
+        //WRITE PROCESS ID ON FILE BY PARENT (SENEDER MANAGER)
+        if(S2==-1){
+            printf("Error Create a process");
+            _exit(1);
+        }
+        pathFileO[15]='7';
+        fileDesc=openFile(pathFileO, modeO);
+        lseek(fileDesc,0L,SEEK_END);
+        char pname[]="S2";
+        writeProcessInfo(fileDesc,pname,S2);
+        close(fileDesc);
+    }
+
+    //CREATE CHILD PROCESS S3
+    pid_t S3;
+    if((S3=fork())==0){
+        pause();
+        exit(0);
+    }
+    else{
+        if(S3==-1){
+            printf("Error Create a process");
+            _exit(1);
+        }
+        //WRITE PROCESS ID ON FILE BY PARENT (SENEDER MANAGER)
+        pathFileO[15]='7';
+        fileDesc=openFile(pathFileO, modeO);
+        lseek(fileDesc,0L,SEEK_END);
+        char pname[]="S3";
+        writeProcessInfo(fileDesc,pname,S3);
+        close(fileDesc);
+    }
+    printf("All child process ready!\n");
+    sleep(1);
+    kill(S1, SIGCONT);
+   /*
+    if((S1=fork())==0){
         
         pathFileS[15]='1';
         char firstLine[]="Id;Message;Id Sender;Id Reciver;Time arrival;Time Depart.\n";
@@ -123,8 +205,7 @@ int main(int argc, char * argv[]) {
         kill(S1,SIGKILL);
         }
     
-    int S2;
-    wait(0);
+    //wait(0);
 
     //CREATE A CHILD PROCESS S2
     if((S2=fork())==0){
@@ -150,8 +231,8 @@ int main(int argc, char * argv[]) {
         S2=getpid();
         kill(S2,SIGKILL);
     }
-    int S3;
-    wait(0);
+    
+
     //CREATE A PROCESS CHILD S3
     if((S3=fork())==0){
         pathFileS[15]='3';
@@ -176,10 +257,69 @@ int main(int argc, char * argv[]) {
         S3=getpid();
         kill(S3,SIGKILL);
     }
-    
+    */
     return 0;
 }
 
+message_info_t getInformationByLine(char* info){
+    int index=0;
+    int lenghtInf=0;
+    message_info_t mes;
+    mes.id=charToInt(readInformation(&index,&lenghtInf,info),&lenghtInf);
+    mes.message=readInformation(&index,&lenghtInf,info);
+    mes.id_sender=readInformation(&index,&lenghtInf,info);
+    mes.id_reciver=readInformation(&index,&lenghtInf, info);
+    mes.del_S1=charToInt(readInformation(&index,&lenghtInf,info),&lenghtInf);
+    mes.del_S2=charToInt(readInformation(&index,&lenghtInf,info),&lenghtInf);
+    mes.del_S3=charToInt(readInformation(&index,&lenghtInf,info),&lenghtInf);
+    mes.type=readInformation(&index,&lenghtInf,info);
+    printf("Read Complete!\n");
+    printf("ID Message: %i\n",mes.id);
+    printf("Message: %s\n",mes.message);
+    printf("id Sender: %s\n",mes.id_sender);
+    printf("id Reciver: %s\n",mes.id_reciver);
+    printf("Del S1: %i\n",mes.del_S1);
+    printf("Del S2: %i\n",mes.del_S2);
+    printf("Del S3: %i\n",mes.del_S3);
+    printf("Type send message: %s\n",mes.type);
+
+}
+
+char* readInformation(int *index, int *lenght, char* info){
+    char *tmp;
+    for(*lenght=0,*index; info[*index]!=';' && info[*index]!='\n'; *index=*index+1,*lenght=*lenght+1){
+        if(*lenght==0){
+            tmp=(char*)malloc(sizeof(char));
+            tmp[*lenght]=info[*index];
+        }
+        else{
+            tmp=realloc(tmp,sizeof(char)*(*lenght+1));
+            tmp[*lenght]=info[*index];
+        }
+    }
+    *index=*index+1;
+    return tmp;
+}
+
+int charToInt(char* string, int *lenght){
+    int res=0;
+    for(int i=*lenght-1; i>=0; i--){
+        if(i==*lenght-1){
+            res=string[i];
+            res=res-48;
+        }
+        else {
+            int n=*lenght-1;
+            int molt=1;
+            while(n!=i){
+                molt=molt*10;
+                --n;
+            }
+            res=res+((int)string[i]*molt);
+        }
+    }
+    return res;
+}
 
 int openFile(char pathFile[], mode_t mode){
     int fd;
@@ -191,14 +331,14 @@ int openFile(char pathFile[], mode_t mode){
     return fd;
 }
 
-void readLine (int fd){
+char* readLine (int fd){
     int l=lenghtLine(fd);
     char *s=(char *)malloc(sizeof(char)*l);
     if(read(fd,s,l)==-1){
         printf("Error read file");
         _exit(1);
     }
-    printf("String read: %s",s);    
+    return s;   
 }
 
 void writeLine(int fd, char string[]){ 
@@ -210,11 +350,10 @@ void writeLine(int fd, char string[]){
 }
 
 void writeProcessInfo(int fd,char name[], int pid){
-    printf("Noi siamo qui\n");
     char *buffer;
     char *tmp=intToChar(pid);
     int lenght;
-    lenght=lenghtString(tmp)+3;
+    lenght=lenghtString(tmp)+4;
     buffer=(char*)malloc(sizeof(char)*lenght);
     for(int i=0; i<lenght; i++){
         if(i<=1){
@@ -224,10 +363,10 @@ void writeProcessInfo(int fd,char name[], int pid){
             buffer[i]=';';
         }
         else {
-            buffer[i]=tmp[i];
+            buffer[i]=tmp[i-3];
         }
     }
-    printf("New String: %s\n",buffer);
+    writeLine(fd,buffer);
 }
 
 char* intToChar(int pid){
@@ -238,59 +377,19 @@ char* intToChar(int pid){
     int i;
     
     for(i=0; tmp!=0; i++){
-        switch(tmp%10){
-            case 0:
-            aint[i]='0';
-            aint=realloc(aint,(i+2)*sizeof(char));
-            break;
-            case 1:
-            aint[i]='1';
-            aint=realloc(aint,(i+2)*sizeof(char));
-            break;
-            case 2:
-            aint[i]='2';
-            aint=realloc(aint,(i+2)*sizeof(char));
-            break;
-            case 3:
-            aint[i]='3';
-            aint=realloc(aint,(i+2)*sizeof(char));
-            break;
-            case 4:
-            aint[i]='4';
-            aint=realloc(aint,(i+2)*sizeof(char));
-            break;
-            case 5:
-            aint[i]='5';
-            aint=realloc(aint,(i+2)*sizeof(char));
-            break;
-            case 6:
-            aint[i]='6';
-            aint=realloc(aint,(i+2)*sizeof(char));
-            break;
-            case 7:
-            aint[i]='7';
-            aint=realloc(aint,(i+2)*sizeof(char));
-            break;
-            case 8:
-            aint[i]='8';
-            aint=realloc(aint,(i+2)*sizeof(char));
-            break;
-            case 9:
-            aint[i]='9';
-            aint=realloc(aint,(i+2)*sizeof(char));
-            break;
-        }
+        aint[i]=((tmp%10)+48);
+        aint=realloc(aint,(i+2)*sizeof(char));
         tmp=tmp/10;
     }
-
-    res=(char*)malloc(sizeof(char)*i);
+    
+    res=(char*)malloc(sizeof(char)*(i+1));
     int j=0;
     i=i-1;
     for(i;i>=0;i--){
         res[j]=aint[i];
         j++;
     }
-
+    res[j]='\n';
     return res;  
 }
 
@@ -298,7 +397,7 @@ int lenghtString(char s[]){
     int i=0;
     for(i; s[i]!='\n'; i++){
     }
-    return i;
+    return (i+1);
 }
 
 int lenghtLine(int fd){
